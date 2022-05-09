f9829ed994038d4f1f7362ac5bee2402e658e95b
Ethan Kreloff <emkreloff@me.com>
2022-05-09 17:25:54 -0500

    Initial Commit

---
+++ AllHealth/AllHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllHealthApp.swift
+//  AllHealth
+//
+//  Created by Ethan Kreloff on 5/9/22.
+//
+
+import SwiftUI
+
+@main
+struct AllHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
